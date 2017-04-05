43f086343cb5f9e2b643c9e0e69ff08c5d463b10
Will Hawkins <willhawkins123@yahoo.com>
2017-04-05 13:57:47 -0400

    Bill Includes Tip, Tax, and Fees

--- src/Bills/Bill.js
+++ src/Bills/Bill.js
@@ -10,6 +10,8 @@ class Bill extends Component {
   render() {
     let total = 0
     let personalTip = 5/this.props.order.personalOrders.length
+    let personalFee = this.props.order.deliveryFee/this.props.order.personalOrders.length
+    console.log(personalFee);
     let personalOrders = this.props.order.personalOrders.map( (personalOrder, index) => {
       let personalTotal = 0
       let items = personalOrder.items.map( (item, index) => {
@@ -18,15 +20,23 @@ class Bill extends Component {
             <p>${item.price.toFixed(2)} {item.name}</p>
         )
       })
-      let personalTax = personalTotal*0.1
+      personalTotal = personalTotal + personalFee
+      let personalTax = personalTotal*(this.props.order.tax/100)
       personalTotal = personalTotal + personalTax + personalTip
       total = total + personalTotal
       return(
         <div>
           <h4>{personalOrder.name}</h4>
-          <ul>{items}</ul>
-          <p>Tax: ${personalTax.toFixed(2)}</p>
-          <p>Personal Tip: ${personalTip.toFixed(2)}</p>
+          <ul>
+            {items}
+            <ul>
+              ${personalFee.toFixed(2)} Personal Fee
+              <br />
+              ${personalTax.toFixed(2)} Tax
+              <br />
+              ${personalTip.toFixed(2)} Personal Tip
+            </ul>
+          </ul>
           <p>Personal Total: ${personalTotal.toFixed(2)}</p>
         </div>
       )
