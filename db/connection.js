510a000697d602ada5f9d5b2561b8d85fc028373
Will Hawkins <willhawkins123@yahoo.com>
2017-04-18 14:27:13 -0400

    Added MongoLab

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/deliverWe")
+mongoose.connect("mongodb://willhawkins:scourge123@ds163010.mlab.com:63010/heroku_bspfb4xt")
 
 const db = mongoose.connection
 
