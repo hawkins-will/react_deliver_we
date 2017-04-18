c862cafaee1184a018efe0c1b31c11aba5f71579
Will Hawkins <willhawkins123@yahoo.com>
2017-04-17 20:12:35 -0400

    Modals added for Adding Items

--- src/MenuItems/MenuItemBoxPersonal.js
+++ src/MenuItems/MenuItemBoxPersonal.js
@@ -16,26 +16,48 @@ class MenuItemBoxPersonal extends Component {
     }
   }
 
+  showModal(index) {
+    document.getElementById(index).style.display='flex'
+  }
+
+  closeModal(index){
+    document.getElementById(index).style.display='none'
+  }
+
   render() {
     let restaurant = this.props.restaurant;
     let menuItems = this.props.restaurant.menuItems.map( (menuItem, index) => {
       return(
-        <div className="personalMenuItemDiv" key={index} onClick={() => {
-          let order = this.props.order;
-          let personalOrder = this.props.personalOrder;
-          let personalId = this.props.personalOrder._id;
-          let newArray = order.personalOrders.filter((personalOrder) => {
-            return personalOrder._id !== personalId
-          })
-          let name = menuItem.name
-          let price = menuItem.price
-          let description = menuItem.description
-          personalOrder.items.push( {name: name, price: price, description: description })
-          newArray.unshift(personalOrder);
-          this.props.handleNewItem(newArray)
-        }}>
-          <p className="personalMenuItemHeader"><span className="personalMenuItemName">{menuItem.name}</span> ${menuItem.price.toFixed(2)}</p>
-          <p className="personalMenuDescription">{menuItem.description}</p>
+        <div>
+          <div className="personalMenuItemDiv" key={index} onClick={ () => this.showModal(index) }>
+            <p className="personalMenuItemHeader"><span className="personalMenuItemName">{menuItem.name}</span> ${menuItem.price.toFixed(2)}</p>
+            <p className="personalMenuDescription">{menuItem.description}</p>
+            <p>{index}</p>
+          </div>
+          <div id={index.toString()} className="modal">
+            <div className="modalContent">
+              <span className="close" onClick={() => this.closeModal(index) }>&times;</span>
+              <div className="innerModal">
+              <p>Add <span className="modalMenuItem">{menuItem.name}</span> to your order?</p>
+              <p>${menuItem.price.toFixed(2)}</p>
+              <div className="confirmButton" onClick={ () => {
+                let order = this.props.order;
+                let personalOrder = this.props.personalOrder;
+                let personalId = this.props.personalOrder._id;
+                let newArray = order.personalOrders.filter((personalOrder) => {
+                  return personalOrder._id !== personalId
+                })
+                let name = menuItem.name
+                let price = menuItem.price
+                let description = menuItem.description
+                personalOrder.items.push( {name: name, price: price, description: description })
+                newArray.unshift(personalOrder);
+                this.props.handleNewItem(newArray)
+                this.closeModal(index)
+              }}>Confirm</div>
+              </div>
+            </div>
+          </div>
         </div>
       )
     })
